503eed801445f98159c5b38ccfc09242b43c7e18
benjewman <bennyadave@gmail.com>
2020-03-05 18:27:49 -0500

    Add p tag in example

--- src/components/Example.js
+++ src/components/Example.js
@@ -11,6 +11,7 @@ export default function Example() {
             <button onClick={() => setCount(count + 1)}>
                 Click me
             </button>
+            <p>Howdy do?!</p>
         </div>
     )
 }
