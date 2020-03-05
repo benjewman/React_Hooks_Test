62c8e0196d4eb8497d5a29e199d1947b14a92acc
benjewman <bennyadave@gmail.com>
2020-03-05 18:42:58 -0500

    Create custom hook to console log count

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import useCountCheck from '../customHooks/useCountCheck'
 
 export default function Example() {
     // create state variable count and the function to update it using hooks
     // useState argument sets the count to 0
     const [count, setCount] = useState(0);
-
+    
+    useCountCheck(count)
+    
     return (
         <div>
             <p>You clicked {count} times.</p>
