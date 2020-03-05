5075c0a935d24909f2579f79f767a9b4c67fecfc
benjewman <bennyadave@gmail.com>
2020-03-05 18:24:15 -0500

    Create example component

---
+++ src/components/Example.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export default function Example() {
+    // create state variable count and the function to update it using hooks
+    // useState argument sets the count to 0
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>You clicked {count} times.</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    )
+}
